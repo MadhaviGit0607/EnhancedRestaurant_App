b6796d4123c7d7d8192e042467b75312b0bb7d2d
MadhaviGit0607 <surepallimadhavi.eee@gmail.com>
2024-08-25 08:01:09 +0000

    solution

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,130 +1,81 @@
-import {Component} from 'react'
+/* eslint-disable import/no-extraneous-dependencies */
+import {useState} from 'react'
 import Cookies from 'js-cookie'
 import {Redirect} from 'react-router-dom'
 
 import './index.css'
 
-class Login extends Component {
-  state = {
-    username: '',
-    password: '',
-    showSubmitError: false,
-    errorMsg: '',
+const Login = props => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const onChangeHandler = event => {
+    const {id, value} = event.target
+    if (id === 'username') {
+      setUsername(value)
+    } else {
+      setPassword(value)
+    }
   }
 
-  onChangeUsername = event => {
-    this.setState({username: event.target.value})
-  }
-
-  onChangePassword = event => {
-    this.setState({password: event.target.value})
-  }
-
-  onSubmitSuccess = jwtToken => {
-    const {history} = this.props
-
-    Cookies.set('jwt_token', jwtToken, {
-      expires: 30,
-    })
+  const onSuccessfulLogin = jwtToken => {
+    const {history} = props
+    Cookies.set('jwt_token', jwtToken, {expires: 1})
     history.replace('/')
   }
 
-  onSubmitFailure = errorMsg => {
-    this.setState({showSubmitError: true, errorMsg})
+  const onFailedLogin = errorMessage => {
+    setErrorMsg(errorMessage)
   }
 
-  submitForm = async event => {
+  const onSubmitLogin = async event => {
     event.preventDefault()
-    const {username, password} = this.state
     const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
+    const api = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
     }
-    const response = await fetch(url, options)
+
+    const response = await fetch(api, options)
     const data = await response.json()
-    if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
+    if (response.ok) {
+      onSuccessfulLogin(data.jwt_token)
     } else {
-      this.onSubmitFailure(data.error_msg)
+      onFailedLogin(data.error_msg)
     }
   }
 
-  renderPasswordField = () => {
-    const {password} = this.state
-
-    return (
-      <>
-        <label className="input-label" htmlFor="password">
-          PASSWORD
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="password-input-field"
-          value={password}
-          onChange={this.onChangePassword}
-          placeholder="Password"
-        />
-      </>
-    )
+  if (Cookies.get('jwt_token')) {
+    return <Redirect to="/" />
   }
 
-  renderUsernameField = () => {
-    const {username} = this.state
-
-    return (
-      <>
-        <label className="input-label" htmlFor="username">
-          USERNAME
-        </label>
+  return (
+    <div className="login-bg">
+      <form onSubmit={onSubmitLogin} className="login-form">
+        <h1 className="login-heading">Login</h1>
+        <label htmlFor="username">USERNAME</label>
         <input
-          type="text"
           id="username"
-          className="username-input-field"
+          type="text"
+          onChange={onChangeHandler}
           value={username}
-          onChange={this.onChangeUsername}
-          placeholder="Username"
         />
-      </>
-    )
-  }
-
-  render() {
-    const {showSubmitError, errorMsg} = this.state
-    const jwtToken = Cookies.get('jwt_token')
-
-    if (jwtToken !== undefined) {
-      return <Redirect to="/" />
-    }
-
-    return (
-      <form>
-        <div className="login-form-container">
-          <img
-            src="https://res.cloudinary.com/dnwwyvtjx/image/upload/v1707810453/food-img_lv5w4z.jpg"
-            className="login-website-logo-mobile-img"
-            alt="website logo"
-          />
-          <img
-            src="https://res.cloudinary.com/dnwwyvtjx/image/upload/v1707810453/food-img_lv5w4z.jpg"
-            className="login-img"
-            alt="website login"
-          />
-        </div>
-        <div className="form-container" onSubmit={this.submitForm}>
-          <h1 className="logo-heading">UNI Resto Cafe</h1>
-          <div className="input-container">{this.renderUsernameField()}</div>
-          <div className="input-container">{this.renderPasswordField()}</div>
-          <button type="submit" className="login-button">
-            Login
-          </button>
-          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
-        </div>
+        <label htmlFor="password">PASSWORD</label>
+        <input
+          id="password"
+          type="password"
+          onChange={onChangeHandler}
+          value={password}
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+        {errorMsg !== '' && <p className="text-danger">{errorMsg}</p>}
       </form>
-    )
-  }
+    </div>
+  )
 }
 
 export default Login
